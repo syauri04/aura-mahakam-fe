1908afe829a10c47590724ed64d01f0524f77e98
syauri04 <syauri04@gmail.com>
2026-05-25 17:38:40 +0700

    update

---
+++ app/kegiatan/page.tsx
@@ -0,0 +1,3 @@
+export default function KegiatanPage() {
+  return <div>List Kegiatan</div>;
+}
---
+++ app/suara-mahakam/page.tsx
@@ -0,0 +1,3 @@
+export default function SuaraMahakamPage() {
+  return <div>List Suara Mahakam</div>;
+}
