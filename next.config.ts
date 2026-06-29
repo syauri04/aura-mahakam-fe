d461923890c767318ef99e6d14264a1f57534f0e
syauri04 <syauri04@gmail.com>
2026-06-29 19:47:59 +0700

    update nextconfig cms.auramahakam.org

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "kabeja.id",
+        hostname: "cms.auramahakam.org",
         pathname: "/uploads/**",
       },
       {
